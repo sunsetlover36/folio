ffa8c28527981b81ee8919b82e451a06a5a0d5f2
sunsetlover36 <sunsetloverrr36@gmail.com>
2023-11-05 03:41:57 +0300

    Init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+export const App = () => {
+  return (
+    <main>
+      <p>hi, i&rsquo;m ruburi.</p>
+      <p>
+        i&nbsp;have 6+&nbsp;years of&nbsp;experience in&nbsp;web engineering.
+      </p>
+      <p>
+        i&nbsp;can do&nbsp;frontend, backend, web3, 3d&nbsp;and mobile apps.
+      </p>
+      <div>
+        <a href="https://t.me/rubyuroboros">telegram</a>
+      </div>
+    </main>
+  );
+};
